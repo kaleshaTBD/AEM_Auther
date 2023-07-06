be0cc1be58f563981046b9778063f58da14f96f1
Kalesha Shaik(UST <187214@ust-global.com>
2023-07-06 18:29:54 +0530

    First Commit_06_07_2023

--- ui.apps/src/main/content/jcr_root/apps/wknd/clientlibs/wknd-clientlibs/js/custom.js
+++ ui.apps/src/main/content/jcr_root/apps/wknd/clientlibs/wknd-clientlibs/js/custom.js
@@ -1,7 +1,27 @@
 function submit_giftcard_natural()
 {
-    var s_name=document.getElementById("sender_name").value;
-    var s_mail=jQuery("#sender_email").val();
-    alert(s_name);
-    alert(s_mail);
+    var form = document.getElementById('giftcardNaturalForm');
+    var data = new FormData(form);
+    const data1 = {};
+    for (const [key, value] of data.entries()) {
+        data1[key] = value;
+    }
+    const jsonData = JSON.stringify(data1);
+    console.log(jsonData);
+      
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = true;
+      
+      xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+          console.log(this.responseText);
+        }
+      });
+      
+      xhr.open("POST", "http://localhost:4502/bin/post/servlet");
+      xhr.setRequestHeader("Authorization", "Basic YWRtaW46YWRtaW4=");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("Cookie", "cq-authoring-mode=TOUCH");
+      
+      xhr.send(jsonData);
 }
